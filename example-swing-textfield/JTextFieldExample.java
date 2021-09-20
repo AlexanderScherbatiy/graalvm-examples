6eb7cefcc32e0d91ce5182a7591f9abf20387679
AlexanderScherbatiy <alexander.scherbatiy@yandex.ru>
2021-09-20 16:56:06 +0300

    Add swing JTextArea example

---
+++ example-swing-textfield/JTextFieldExample.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class JTextFieldExample {
+
+    public static void main(String[] args) throws Exception {
+
+        SwingUtilities.invokeAndWait(() -> {
+
+            JFrame frame = new JFrame("Hello World");
+
+            JTextField textField = new JTextField();
+
+            JPanel panel = new JPanel(new BorderLayout());
+            panel.add(textField, BorderLayout.CENTER);
+
+            frame.add(panel);
+            frame.setSize(400, 300);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
+
---
+++ example-swing-textfield/run.sh
@@ -0,0 +1,7 @@
+ROOTDIR=$(dirname "$0")/..
+
+export APP=JTextFieldExample
+export OPTS="-Djava.awt.headless=false"
+
+$ROOTDIR/run.sh
+
