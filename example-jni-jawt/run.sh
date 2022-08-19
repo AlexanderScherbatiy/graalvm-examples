f4e2e58d5e44348705641ade75d4c4ff0968aa9a
AlexanderScherbatiy <alexander.scherbatiy@bell-sw.com>
2022-08-19 16:31:11 +0300

    Link jawt lib statically for JAWT example

--- example-jni-jawt/run.sh
+++ example-jni-jawt/run.sh
@@ -4,9 +4,9 @@ ROOTDIR=$(dirname "$0")/..
 
 export APP=JAWTExample
 export USE_JNI=true
-export JNI_OPTIONS="-ljawt -L$GRAALVM/lib"
+export JNI_OPTIONS="-l:libjawt.a -l:libawt_xawt.a -l:libawt.a -L$GRAALVM/lib"
 export RUN_APP=true
-export NATIVE_IMAGE=false
+export NATIVE_IMAGE=true
 export OUT=.
 
 $ROOTDIR/run.sh
